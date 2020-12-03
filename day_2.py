8a505d6332b91eb163f45cdd679ab3c6cb9862a3
Catbug Developer <catbugdeveloper@gmail.com>
2020-12-03 04:09:52 +0100

    Day 2, part 2

--- day_2.py
+++ day_2.py
@@ -1,16 +1,19 @@
+import typing
+
 data = []
 
-_file = open('day_2.txt', 'r') 
+_file = open("day_2.txt", "r")
 lines = _file.readlines()
 
 for line in lines:
     data.append(line.strip())
 
+
 def is_password_correct(_line: str) -> bool:
     _range, _char, _password = _line.split()
-    
-    _range = [*map(lambda el: int(el), _range.split('-'))]
-    _char = _char.replace(':', '')
+
+    _range = [*map(lambda el: int(el), _range.split("-"))]
+    _char = _char.replace(":", "")
     _password = _password.strip()
 
     password_as_list = [char for char in _password]
@@ -22,13 +25,36 @@ def is_password_correct(_line: str) -> bool:
 
     return count >= _range[0] and count <= _range[1]
 
-def calculate_correct_passwords(_data:list) -> int:
-    count = 0 
+
+def is_password_correct_positional(_line: str) -> bool:
+    _positions, _char, _password = _line.split()
+
+    _positions = [*map(lambda el: int(el), _positions.split("-"))]
+    _char = _char.replace(":", "")
+    _password = _password.strip()
+
+    password_as_list = [char for char in _password]
+
+    first = _positions[0] - 1
+    last = _positions[1] - 1
+
+    if (password_as_list[first] == _char and password_as_list[last] != _char) or (
+        password_as_list[first] != _char and password_as_list[last] == _char
+    ):
+        return True
+
+    return False
+
+
+def calculate_correct_passwords(_fn: typing.Callable[[str], bool], _data: list) -> int:
+    count = 0
 
     for line in _data:
-        if is_password_correct(line):
+        if _fn(line):
             count += 1
-    
+
     return count
 
-print(calculate_correct_passwords(_data=data))
+
+print(calculate_correct_passwords(_fn=is_password_correct, _data=data))
+print(calculate_correct_passwords(_fn=is_password_correct_positional, _data=data))
